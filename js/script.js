f2b38d6de223dd7ca7c0cb8aba40895c07befa91
Albert I Palacios <albertpalacios70@yahoo.com>
2024-02-19 14:26:59 +0000

    developed script to allow for english to hindi translation utilizing a rest api.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text");
+selectTag = document.querySelectorAll("select");
+translateBtn = document.querySelector("button");
 
-selectTag.forEach(tag => {
+selectTag.forEach((tag, id) => {
     for (const country_code in countries) {
-        console.log(countries[country_code]);
+        let selected;
+        if(id == 0 && country_code == "en-GB"){
+            selected = "selected";
+        } else if (id == 1 && country_code == "hi-IN"){
+            selected = "selected";
+        }
         let option = `<option value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option); //adding options tag inside select tag
     }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+    translateFrom = selectTag[0].value, // getting fromSelect tag value
+    translateTo = selectTag[1].value;   // getting toSelect tag value
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    // fetching api response and returning it with parsing into js obj
+    // add in another then method receiving that obj
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+    });
 });
